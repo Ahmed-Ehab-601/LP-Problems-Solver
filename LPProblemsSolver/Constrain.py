ae021b05ebd75159f8e8b159e1d1c94a28c0e642
samaa Ibrahim <es-samaa.ibrahim2027@alexu.edu.eg>
2025-03-17 02:39:45 +0200

    Constrain

---
+++ LPProblemsSolver/Constrain.py
@@ -0,0 +1,13 @@
+
+class Constrain :
+    def __init__(self,coef,type,solution,priority):
+        self.coef=coef
+        self.type=type
+        self.solution =solution
+        self.priority= priority
+
+   
+
+
+
+
